46fe646080e7517cb5d068a5df9c952dddb07de9
Csaba Janos Kigyosi <csabronc@gmail.com>
2018-03-29 23:29:46 +0200

    init

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,53 +4,53 @@ import { Link } from 'react-router-dom';
 import Payments from './Payments';
 
 class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
-      case null:
-        return;
-      case false:
-        return (
-          <li>
-            <a href="/auth/google"> Sign in with google</a>
-          </li>
-        );
-      // user is logged in
-      default:
-        return [
-          <li key="1">
-            <Payments />
-          </li>,
-          <li key="3" style={{ margin: '0 10px' }}>
-            Credits: {this.props.auth.credits}
-          </li>,
-          <li key="2">
-            <a href="/api/logout"> Log out</a>
-          </li>
-        ];
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google"> Sign in with google</a>
+                    </li>
+                );
+            // user is logged in
+            default:
+                return [
+                    <li key="1">
+                        <Payments />
+                    </li>,
+                    <li key="3" style={{ margin: '0 10px' }}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="2">
+                        <a href="/api/logout"> Log out</a>
+                    </li>
+                ];
+        }
     }
-  }
 
-  render() {
-    return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link
-            to={this.props.auth ? '/surveys' : '/'}
-            className="left brand-logo"
-          >
-            Emaily
-          </Link>
-          <ul className="right">
-            <li>{this.renderContent()}</li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
+                        Restaurant
+                    </Link>
+                    <ul className="right">
+                        <li>{this.renderContent()}</li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
 }
 
 function mapStateToProps({ auth }) {
-  return { auth };
+    return { auth };
 }
 
 // function mapStateToProps(state) {
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -14,11 +15,11 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(
-  cookieSession({
-    //30 days before it will automatically expire
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+    cookieSession({
+        //30 days before it will automatically expire
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
 
 app.use(passport.initialize());
@@ -26,17 +27,18 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
-  // Express will serve up the index.html file
-  // if it doesn't recoginize the route.
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+    // Express will serve up the index.html file
+    // if it doesn't recoginize the route.
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const PORT = process.env.PORT || 5000;
